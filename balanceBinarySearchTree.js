a72e6f402a23508ee84f070eeea9a5c7f9191542
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-26 16:09:09 +0500

    create function removeDuplictes that removes duplicate value in an array

--- balanceBinarySearchTree.js
+++ balanceBinarySearchTree.js
@@ -7,8 +7,22 @@ function Node(data) {
 }
 
 function Tree(array) {
-	let root = buildTree(array, 0, array.length - 1);
+	let root = buildTree(array);
 	return {
 		root,
 	};
 }
+
+
+
+function removeDuplicates(array) {
+	let newArray = [];
+	array = array.forEach((element) => {
+		if (!newArray.includes(element)) {
+			newArray.push(element);
+		}
+	});
+	return newArray;
+}
+
+let data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
